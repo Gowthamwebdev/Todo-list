c33cc32d65a8607a2efc3d2015927307087582da
gowtham <gowthamgalaxy007@gmail.com>
2024-05-21 11:36:33 +0530

    Styles were updated

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -1,24 +1,24 @@
-let toDoInput; // a place where the user enters the task content
-let errorInfo; // information about lack of tasks / need to enter text
-let addBtn; // ADD button - adds new items to the list
-let ulList; // list of tasks, such as UL
-let newToDo; // newly added li, new task
+let toDoInput; 
+let errorInfo;
+let addBtn; 
+let ulList; 
+let newToDo;
 
-let popup; // popup
-let popupInfo; // test in popup, what if you add text
-let todoToEdit; // edited Todo
-let popupInput; // input in the popup
-let popupAddBtn; // 'accept' button in the popup
-let popupCloseBtn; // 'cancel' button in the popup
+let popup; 
+let popupInfo; 
+let todoToEdit; 
+let popupInput; 
+let popupAddBtn;
+let popupCloseBtn;
 
 const main = () => {
-    // call our functions
+    
     prepareDOMElements();
     prepareDOMEvents();
 }
 
 const prepareDOMElements = () => {
-    // get all elements
+    
     toDoInput = document.querySelector('.input-el');
     errorInfo = document.querySelector('.error-el');
     addBtn = document.querySelector('.add-btn');
@@ -32,7 +32,7 @@ const prepareDOMElements = () => {
 }
 
 const prepareDOMEvents = () => {
-    // assign event listeners
+    
     addBtn.addEventListener('click', addNewToDo);
     ulList.addEventListener('click', checkClick);
     popupCloseBtn.addEventListener('click', closePopup);
@@ -41,16 +41,16 @@ const prepareDOMEvents = () => {
 }
 
 const addNewToDo = () => {
-    // create a new todo
+    
     if (toDoInput.value != '') {
         newToDo = document.createElement('li');
         newToDo.textContent = toDoInput.value;
-        // add todo tools (buttons)
+        
         createToolAreal();
-        // add our todo to the ul list
+       
         ulList.append(newToDo);
 
-        // clear error and input after adding todo
+        
         toDoInput.value = '';
         errorInfo.textContent = '';
     } else {
@@ -59,10 +59,10 @@ const addNewToDo = () => {
 }
 
 const createToolAreal = () => {
-    // create elements (buttons) and add them to todo
+    
     const div = document.createElement('div');
     div.classList.add('tools');
-    // add our tools to the new todo
+    
     newToDo.append(div);
 
     const buttonDone = document.createElement('button');
@@ -77,11 +77,11 @@ const createToolAreal = () => {
     buttonCancel.classList.add('delete');
     buttonCancel.innerHTML = '<i class="fas fa-times"></i>'
 
-    // add elements together 
+    
     div.append(buttonDone, buttonEdit, buttonCancel);
 }
 
-// function checking what we click on (to know whether to finish the task, delete it, or edit it)
+
 const checkClick = (e) => {
     if (e.target.matches('.complete')) {
         e.target.closest('li').classList.toggle('completed');
@@ -96,7 +96,7 @@ const checkClick = (e) => {
     }
 }
 
-// functions responsible for the popup
+
 const editToDo = (e) => {
     todoToEdit = e.target.closest('li');
     popupInput.value = todoToEdit.firstChild.textContent;
@@ -108,7 +108,7 @@ const closePopup = () => {
     popupInfo.textContent = '';
 }
 
-// function introducing changes from the input to our li element
+
 const changeTodoText = () => {
     if (popupInput.value != '') {
         todoToEdit.firstChild.textContent = popupInput.value;
@@ -123,19 +123,16 @@ const changeTodoText = () => {
 const deleteToDo = (e) => {
     e.target.closest('li').remove();
 
-    // display no items when there are none
     const allToDos = ulList.querySelectorAll('li');
     if (allToDos.length == 0) {
         errorInfo.textContent = 'No tasks on the list.'
     }
 }
 
-// also add todo on enter
 const enterKeyCheck = (e) => {
     if (e.key == 'Enter') {
         addNewToDo();
     }
 }
 
-// executed when the page changes
 document.addEventListener('DOMContentLoaded', main);
